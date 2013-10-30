6576ea3016f5513aa839198a7ebd07f48a97de5f
Matt Erhard <matthew.erhard@egov.com>
2013-10-30 09:57:33 -0400

    Add dynamic title to static pages

--- spec/features/static_pages_spec.rb
+++ spec/features/static_pages_spec.rb
@@ -3,26 +3,38 @@ require 'spec_helper'
 describe "Static pages" do
 
   describe "Home page" do
+    before(:each) { visit static_pages_home_path }
 
     it "should have the content 'Sample App'" do
-      visit static_pages_home_path
       expect(page).to have_content('Sample App')
+    end
+
+    it "should have the right title" do
+      expect(page).to have_title("Ruby on Rails Tutorial Sample App | Home")
     end
   end
 
   describe "Help page" do
+    before(:each) { visit static_pages_help_path }
 
     it "should have the content 'Help'" do
-      visit static_pages_help_path
       expect(page).to have_content('Help')
+    end
+
+    it "should have the right title" do
+      expect(page).to have_title("Ruby on Rails Tutorial Sample App | Help")
     end
   end
 
   describe "About page" do
+    before(:each) { visit static_pages_about_path }
 
     it "should have the content 'About Us'" do
-      visit static_pages_about_path
       expect(page).to have_content('About Us')
+    end
+
+    it "should have the right title" do
+      expect(page).to have_title("Ruby on Rails Tutorial Sample App | About Us")
     end
   end
 end
